3a14ac2a3db92a14a4a4fbbad8117c15e185269f
dom96 <dominikpicheta@googlemail.com>
2010-07-09 01:47:33 +0100

    Added /nyc, now playing with color.

--- xchat_exaile.py
+++ xchat_exaile.py
@@ -1,7 +1,7 @@
 import xchat, sys, dbus
 __module_name__ = "Exaile" 
 __module_version__ = "1.0" 
-__module_description__ = "Exaile now playing script" 
+__module_description__ = "Exaile now playing script with some other cool features" 
 
 def getSongInfo():
   bus = dbus.SessionBus()
@@ -28,17 +28,23 @@ def getSongInfo():
 
 
 def printSong(word, word_eol, userdata):
-
   songInfo = getSongInfo()
   if songInfo == 0:
     xchat.prnt("Exaile is not playing")
   elif songInfo == 1:
     xchat.prnt("Exaile is not running")
   else:
-    xchat.command("me is listening to %s by %s - %s (%s/%s)" % songInfo)
+    if not userdata:
+      xchat.command("me is listening to %s by %s - %s (%s/%s)" % songInfo)
+    else:
+      xchat.command("me is listening to \x0303%s\x03 by \x0303%s\x03 - \x0303%s\x03 (\x0305%s\x03/\x0305%s\x03)" % songInfo)
   
   return xchat.EAT_ALL
 
+
+
 xchat.prnt("Exaile script initialized")
 xchat.prnt("Use /np to announce the currently played song")
-xchat.hook_command("np", printSong)
+xchat.hook_command("np", printSong, False)
+xchat.hook_command("npc", printSong, True)
+
