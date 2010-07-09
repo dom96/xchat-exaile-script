0adf9f428ac0c9bb416848e4e87e7c5108b631df
dom96 <dominikpicheta@googlemail.com>
2010-07-09 02:31:57 +0100

    Added exaile_ver, tried getting exnext and exprev to work, but it doesn't.

--- xchat_exaile.py
+++ xchat_exaile.py
@@ -3,8 +3,9 @@ __module_name__ = "Exaile"
 __module_version__ = "1.0" 
 __module_description__ = "Exaile now playing script with some other cool features" 
 
+bus = dbus.SessionBus()
+
 def getSongInfo():
-  bus = dbus.SessionBus()
   try:
     remote_object = bus.get_object("org.exaile.Exaile","/org/exaile/Exaile")
     iface = dbus.Interface(remote_object, "org.exaile.Exaile")
@@ -41,10 +42,35 @@ def printSong(word, word_eol, userdata):
   
   return xchat.EAT_ALL
 
+def printExaileVersion(word, word_eol, userdata):
+  try:
+    remote_object = bus.get_object("org.exaile.Exaile","/org/exaile/Exaile")
+    iface = dbus.Interface(remote_object, "org.exaile.Exaile")
 
+    xchat.command("me is listening to his awesome tunes using Exaile " + str(iface.GetVersion()))
+  except dbus.exceptions.DBusException as Err:
+    xchat.prnt("An error occured, but xchat crashes when it gets printed.")
+    #xchat.prnt(Err.get_dbus_message()) # Unsafe *cough*
+
+def chooseSong(word, word_eol, next):
+  try:
+    remote_object = bus.get_object("org.exaile.Exaile","/org/exaile/Exaile")
+    iface = dbus.Interface(remote_object, "org.exaile.Exaile")
+
+    if next:
+      iface.next_track()
+    else:
+      iface.prev_track()
+      
+  except dbus.exceptions.DBusException as Err:
+    xchat.prnt("An error occured, but xchat crashes when it gets printed.")
+    #xchat.prnt(Err.get_dbus_message()) # Unsafe *cough*
 
 xchat.prnt("Exaile script initialized")
 xchat.prnt("Use /np to announce the currently played song")
 xchat.hook_command("np", printSong, False)
 xchat.hook_command("npc", printSong, True)
+xchat.hook_command("exaile_ver", printExaileVersion)
+#xchat.hook_command("exnext", chooseSong, True)
+#xchat.hook_command("exprev", chooseSong, False)
 
